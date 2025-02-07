ed9a7c5aa2581f3c18a92098b07cbd59f65dab0b
Tritonn204 <tritonn204@gmail.com>
2025-02-07 15:03:10 -0800

    altered building + ci processes for static linking + app bundling

--- core/build.rs
+++ core/build.rs
@@ -3,6 +3,7 @@ use vergen::EmitBuilder;
 
 // https://docs.rs/vergen/latest/vergen/struct.EmitBuilder.html#method.emit
 fn main() -> Result<(), Box<dyn Error>> {
+  static_vcruntime::metabuild();
   EmitBuilder::builder()
     .all_build()
     .all_cargo()
